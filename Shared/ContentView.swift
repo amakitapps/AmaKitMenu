a2296e90429976b8d7e479ad89d3fb9e0d8e63c3
Kenneth Jackson <kjackson@connectionsis.com>
2020-07-17 18:53:14 -0500

    Initial Commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var menuItems = [
+        Menu(name: "Chicken Salad", image: "chickensalad"),
+        Menu(name: "Reuben", image: "reuben"),
+        Menu(name: "Chicken Fried Steak", image: "chickenfriedsteak"),
+        Menu(name: "Frito Pie", image: "fritopie")
+    ]
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            List(menuItems, id: \.id) { item in
+                HStack {
+                    Image(item.image)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .cornerRadius(5)
+                    Text(item.name)
+                }
+            }
+            .navigationBarTitle("Kenneth's Cozy Cafe")
+        }
+    }
+}
+
+struct Menu: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+}
+
+
+struct MenuDetail: View {
+    var menu: Menu
+    
+    var body: some View {
+        VStack {
+            Image(menu.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 40, height: 40)
+                .cornerRadius(5)
+            Text(menu.name)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+            Spacer()
+        }
     }
 }
 
