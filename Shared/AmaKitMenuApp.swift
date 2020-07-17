8e1e01dd32ce648c29d2cf7d9d066e1a685cf752
Kenneth Jackson <kjackson@connectionsis.com>
2020-07-17 17:49:16 -0500

    Initial Commit

---
+++ Shared/AmaKitMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmaKitMenuApp.swift
+//  Shared
+//
+//  Created by Kenneth Jackson on 7/17/20.
+//
+
+import SwiftUI
+
+@main
+struct AmaKitMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
